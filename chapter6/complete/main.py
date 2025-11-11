117e805db7d32796b584ba116945b93aa08f56c9
Fan Fan <92854302+ProfessorErdo@users.noreply.github.com>
2025-11-11 10:23:59 +0800

    Change health check message to 'Dedication Corner Forever!!!'

--- chapter6/complete/main.py
+++ chapter6/complete/main.py
@@ -51,7 +51,7 @@ def get_db():
     tags=["analytics"],
 )
 async def root():
-    return {"message": "API health check successful"}
+    return {"message": "Dedication Corner Forever!!!"}
 
 
 @app.get(
